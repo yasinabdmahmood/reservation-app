d3bcdfefc7f6d9648cda13f5d820197a0a94499e
Yaseen <abd181103@gmail.com>
2025-08-16 12:33:16 +0300

    Add app.db to gitignore and add route redirect

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('index', { title: 'Home' }));
+
+// Redirect root to /students/new
+router.get('/', (req, res) => {
+  res.redirect('/students/new');
+});
 
 router.use('/students', require('./studentRoutes'));
 router.use('/admin', require('./adminRoutes')); 
